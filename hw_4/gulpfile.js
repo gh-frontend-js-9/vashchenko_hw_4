2494ca8924341f257c92a43e625c5ca8c3ff4623
Alex <54575097+WebInfUa@users.noreply.github.com>
2019-11-05 12:43:27 +0200

    Work on the mistakes

--- hw_4/gulpfile.js
+++ hw_4/gulpfile.js
@@ -4,17 +4,9 @@ const gulpSource = require('gulp-sourcemaps');
 
 gulp.task('sass-watch', function () {
   return gulp.src('./src/sass/**/*.scss')
-    .pipe(gulpSource.init()) //Показывает строку scss в консоли
+    .pipe(gulpSource.init())
     .pipe(gulpSass({outputStyle: 'compressed'}).on('Error', gulpSass.logError))
-    /**
-     * nested - вложенный, по умолчанию;
-     * expanded - развернутый;
-     * compact - компактный, когда селектор и его свойства в фигурных скобках выводятся в одну строку;
-     * compressed - сжатый. Кроме того, благодаря обработке .on('error', sass.logError), если возникнет ошибка,
-     * нам не придется перезагружать команду выполенния Gulpfile и мы будем видеть, в какой строке Sass файла у нас ошибка.
-     * В примерах я буду использовать стиль вывода expanded для наглядности.
-     **/
-    .pipe(gulpSource.write('./')) //Показывает строку scss в консоле
+    .pipe(gulpSource.write('./'))
     .pipe(gulp.dest('./css/'))
 });
 
